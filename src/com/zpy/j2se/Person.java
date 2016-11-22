c82cad5c37f61f998e2d34e9000c10457cf75448
pingze <1952211498@qq.com>
2016-11-22 14:22:40 +0800

    增加属性name

--- src/com/zpy/j2se/Person.java
+++ src/com/zpy/j2se/Person.java
@@ -2,4 +2,13 @@ package com.zpy.j2se;
 
 public class Person {
 
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
